19871969a1bccab9621a11a04fbba0777fff7ed5
mike2202th <140124553+mike2202th@users.noreply.github.com>
2026-07-22 20:16:13 -0400

    Add files via upload

---
+++ src/formulariografico/FormularioGrafico.java
@@ -0,0 +1,14 @@
+
+package formulariografico;
+
+public class FormularioGrafico {
+
+    public static void main(String[] args) {
+        ProcesarDatos datos = new ProcesarDatos();
+        
+        datos.capturarDatos();
+        
+        datos.PresentarDatos();
+    }
+    
+}
